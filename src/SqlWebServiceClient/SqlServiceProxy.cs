b2605efc582230decb7d913008730d687ea61905
Your Name <anton.piruev.97@yandex.com>
2026-03-28 18:24:35 +0300

    Fixed SQL connection error

--- src/SqlWebServiceClient/SqlServiceProxy.cs
+++ src/SqlWebServiceClient/SqlServiceProxy.cs
@@ -3,52 +3,57 @@ using System.ServiceModel;
 
 namespace SqlWebServiceClient
 {
-    [System.Runtime.Serialization.DataContract]
+    [System.Runtime.Serialization.DataContract(Name = "ConnectRequestType", Namespace = "http://sqlwebservice.local/v1")]
     public class ConnectRequest
     {
-        [System.Runtime.Serialization.DataMember] public string Server                { get; set; }
-        [System.Runtime.Serialization.DataMember] public string Database              { get; set; } = "master";
-        [System.Runtime.Serialization.DataMember] public bool   UseIntegratedSecurity { get; set; } = true;
-        [System.Runtime.Serialization.DataMember] public string Username              { get; set; }
-        [System.Runtime.Serialization.DataMember] public string Password              { get; set; }
-        [System.Runtime.Serialization.DataMember] public int    ConnectTimeoutSeconds { get; set; } = 30;
+        [System.Runtime.Serialization.DataMember(Order = 1)] public string Server                { get; set; }
+        [System.Runtime.Serialization.DataMember(Order = 2)] public string Database              { get; set; } = "master";
+        [System.Runtime.Serialization.DataMember(Order = 3)] public bool   UseIntegratedSecurity { get; set; } = true;
+        [System.Runtime.Serialization.DataMember(Order = 4)] public string Username              { get; set; }
+        [System.Runtime.Serialization.DataMember(Order = 5)] public string Password              { get; set; }
+        [System.Runtime.Serialization.DataMember(Order = 6)] public int    ConnectTimeoutSeconds { get; set; } = 30;
     }
 
-    [System.Runtime.Serialization.DataContract]
+    [System.Runtime.Serialization.DataContract(Name = "ConnectResponseType", Namespace = "http://sqlwebservice.local/v1")]
     public class ConnectResponse
     {
-        [System.Runtime.Serialization.DataMember] public bool   Success   { get; set; }
-        [System.Runtime.Serialization.DataMember] public string SessionId { get; set; }
-        [System.Runtime.Serialization.DataMember] public string Message   { get; set; }
+        [System.Runtime.Serialization.DataMember(Order = 1)] public bool   Success   { get; set; }
+        [System.Runtime.Serialization.DataMember(Order = 2)] public string SessionId { get; set; }
+        [System.Runtime.Serialization.DataMember(Order = 3)] public string Message   { get; set; }
     }
 
-    [System.Runtime.Serialization.DataContract]
+    [System.Runtime.Serialization.DataContract(Name = "SqlVersionResponseType", Namespace = "http://sqlwebservice.local/v1")]
     public class SqlVersionResponse
     {
-        [System.Runtime.Serialization.DataMember] public bool   Success { get; set; }
-        [System.Runtime.Serialization.DataMember] public string Version { get; set; }
-        [System.Runtime.Serialization.DataMember] public string Message { get; set; }
+        [System.Runtime.Serialization.DataMember(Order = 1)] public bool   Success { get; set; }
+        [System.Runtime.Serialization.DataMember(Order = 2)] public string Version { get; set; }
+        [System.Runtime.Serialization.DataMember(Order = 3)] public string Message { get; set; }
     }
 
-    [System.Runtime.Serialization.DataContract]
+    [System.Runtime.Serialization.DataContract(Name = "DisconnectResponseType", Namespace = "http://sqlwebservice.local/v1")]
     public class DisconnectResponse
     {
-        [System.Runtime.Serialization.DataMember] public bool   Success { get; set; }
-        [System.Runtime.Serialization.DataMember] public string Message { get; set; }
+        [System.Runtime.Serialization.DataMember(Order = 1)] public bool   Success { get; set; }
+        [System.Runtime.Serialization.DataMember(Order = 2)] public string Message { get; set; }
     }
 
-    [System.Runtime.Serialization.DataContract]
+    [System.Runtime.Serialization.DataContract(Name = "DisconnectRequestType", Namespace = "http://sqlwebservice.local/v1")]
     public class DisconnectRequest
     {
-        [System.Runtime.Serialization.DataMember] public string SessionId { get; set; }
+        [System.Runtime.Serialization.DataMember(Order = 1)] public string SessionId { get; set; }
     }
 
     [ServiceContract(Namespace = "http://sqlwebservice.local/v1")]
     public interface ISqlService
     {
-        [OperationContract] ConnectResponse    Connect(ConnectRequest request);
-        [OperationContract] SqlVersionResponse GetSqlVersion(string sessionId);
-        [OperationContract] DisconnectResponse Disconnect(DisconnectRequest request);
+        [OperationContract(Name = "Connect")]
+        ConnectResponse Connect([System.ServiceModel.MessageParameter(Name = "request")] ConnectRequest request);
+
+        [OperationContract(Name = "GetSqlVersion")]
+        SqlVersionResponse GetSqlVersion([System.ServiceModel.MessageParameter(Name = "sessionId")] string sessionId);
+
+        [OperationContract(Name = "Disconnect")]
+        DisconnectResponse Disconnect([System.ServiceModel.MessageParameter(Name = "request")] DisconnectRequest request);
     }
 
     /// <summary>
